d4bf76fb9d3cb6b1285d04caf221ed3a0d20fb90
Urszuly, László <urszuly.laszlo@hoisthq.com>
2014-04-10 10:52:51 +0200

    Disabled Unicode character escaping in the ItemToJsonParser.

--- sdk/src/com/podio/sdk/parser/ItemToJsonParser.java
+++ sdk/src/com/podio/sdk/parser/ItemToJsonParser.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.podio.sdk.Parser;
 
 public class ItemToJsonParser implements Parser<Object> {
@@ -13,7 +14,9 @@ public class ItemToJsonParser implements Parser<Object> {
         List<String> result = new ArrayList<String>(1);
 
         if (source != null) {
-            Gson gson = new Gson();
+            GsonBuilder builder = new GsonBuilder();
+            builder.disableHtmlEscaping();
+            Gson gson = builder.create();
             String json = gson.toJson(source);
             result.add(json);
         }
