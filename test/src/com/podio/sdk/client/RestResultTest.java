3702bb4264144eed2eef21496d3998b1a109a990
Urszuly, László <urszuly.laszlo@hoisthq.com>
2014-04-26 07:51:10 +0200

    Added tests for RestResult.

--- test/src/com/podio/sdk/client/RestResultTest.java
+++ test/src/com/podio/sdk/client/RestResultTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import android.test.AndroidTestCase;
 
+import com.podio.sdk.Session;
+
 public class RestResultTest extends AndroidTestCase {
 
     /**
@@ -27,6 +29,36 @@ public class RestResultTest extends AndroidTestCase {
 
         RestResult target = new RestResult(isSuccess, message, item);
         assertNotNull(target);
+        assertNull(target.session());
+        assertEquals(isSuccess, target.isSuccess());
+        assertEquals(message, target.message());
+        assertEquals(item, target.item());
+    }
+
+    /**
+     * Verifies that a {@link RestResult} created with a successful status, a
+     * changed session object, a non-empty message and a non-empty list of
+     * items, keeps the initial values and returns them correctly when
+     * requested.
+     * 
+     * <pre>
+     * 
+     * 1. Create a new RestResult item with known, positive values.
+     * 
+     * 2. Verify that the values don't get distorted until they are
+     *      requested again.
+     * 
+     * </pre>
+     */
+    public void testCreateRestResultSuccessSessionMessageItems() {
+        boolean isSuccess = true;
+        Session session = new Session("accessToken", "refreshToken", 1L);
+        String message = "success";
+        Object item = new Object();
+
+        RestResult target = new RestResult(isSuccess, session, message, item);
+        assertNotNull(target);
+        assertNotNull(target.session());
         assertEquals(isSuccess, target.isSuccess());
         assertEquals(message, target.message());
         assertEquals(item, target.item());
@@ -53,6 +85,7 @@ public class RestResultTest extends AndroidTestCase {
 
         RestResult target = new RestResult(isSuccess, message, items);
         assertNotNull(target);
+        assertNull(target.session());
         assertEquals(isSuccess, target.isSuccess());
         assertEquals(message, target.message());
         assertEquals(items, target.item());
