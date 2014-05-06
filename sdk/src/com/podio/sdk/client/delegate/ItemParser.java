a8f8a9d66098336846f40d164ca3f6b707857e3a
Urszuly, László <urszuly.laszlo@hoisthq.com>
2014-05-06 10:24:13 +0200

    Injected custom parsing of Field objects.
    It's actually the "values" attribute in the Field object that needs a conditional
    parsing depending on what value the "type" attribute holds.

--- sdk/src/com/podio/sdk/client/delegate/ItemParser.java
+++ sdk/src/com/podio/sdk/client/delegate/ItemParser.java
@@ -22,8 +22,28 @@
 
 package com.podio.sdk.client.delegate;
 
+import java.lang.reflect.Type;
+
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.podio.sdk.domain.Item.CalculationField;
+import com.podio.sdk.domain.Item.CategoryField;
+import com.podio.sdk.domain.Item.ContactField;
+import com.podio.sdk.domain.Item.DateField;
+import com.podio.sdk.domain.Item.DurationField;
+import com.podio.sdk.domain.Item.EmbedField;
+import com.podio.sdk.domain.Item.Field;
+import com.podio.sdk.domain.Item.ImageField;
+import com.podio.sdk.domain.Item.LocationField;
+import com.podio.sdk.domain.Item.MoneyField;
+import com.podio.sdk.domain.Item.NumberField;
+import com.podio.sdk.domain.Item.ProgressField;
 import com.podio.sdk.internal.utils.Utils;
 
 /**
@@ -37,6 +57,51 @@ import com.podio.sdk.internal.utils.Utils;
  * 
  */
 public class ItemParser<T> {
+
+    private static final class FieldDeserializer implements JsonDeserializer<Field> {
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * com.google.gson.JsonDeserializer#deserialize(com.google.gson.JsonElement
+         * , java.lang.reflect.Type, com.google.gson.JsonDeserializationContext)
+         */
+        @Override
+        public Field deserialize(JsonElement element, Type type,
+                JsonDeserializationContext gsonContext) throws JsonParseException {
+
+            JsonObject jsonObject = element != null ? element.getAsJsonObject() : null;
+            JsonElement fieldType = jsonObject != null ? jsonObject.get("type") : null;
+            String fieldTypeName = fieldType != null ? fieldType.getAsString() : null;
+
+            if ("calculation".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, CalculationField.class);
+            } else if ("category".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, CategoryField.class);
+            } else if ("contact".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, ContactField.class);
+            } else if ("date".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, DateField.class);
+            } else if ("duration".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, DurationField.class);
+            } else if ("embed".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, EmbedField.class);
+            } else if ("image".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, ImageField.class);
+            } else if ("location".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, LocationField.class);
+            } else if ("money".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, MoneyField.class);
+            } else if ("number".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, NumberField.class);
+            } else if ("progress".equals(fieldTypeName)) {
+                return gsonContext.deserialize(element, ProgressField.class);
+            } else {
+                return new Field();
+            }
+        }
+    }
 
     private final Class<T> classOfItem;
 
@@ -56,8 +121,11 @@ public class ItemParser<T> {
         T result = null;
 
         if (Utils.notEmpty(source)) {
-            Gson gson = new Gson();
-            result = gson.fromJson(source, classOfItem);
+            result = new GsonBuilder() //
+                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES) //
+                    .registerTypeAdapter(Field.class, new FieldDeserializer()) //
+                    .create() //
+                    .fromJson(source, classOfItem);
         }
 
         return result;
--- sdk/src/com/podio/sdk/domain/Item.java
+++ sdk/src/com/podio/sdk/domain/Item.java
@@ -22,6 +22,8 @@
 
 package com.podio.sdk.domain;
 
+import android.renderscript.Sampler.Value;
+
 import com.podio.sdk.domain.helper.AccountInfo;
 import com.podio.sdk.domain.helper.FileInfo;
 import com.podio.sdk.domain.helper.ParticipantInfo;
@@ -34,15 +36,9 @@ import com.podio.sdk.domain.helper.ValueInfo;
 public final class Item {
 
     public static final class Activity {
-        private Activity() {
-            // Hide the constructor.
-        }
     }
 
     public static final class Client {
-        private Client() {
-            // Hide the constructor.
-        }
     }
 
     public static final class Config {
@@ -55,33 +51,179 @@ public final class Item {
         public final Boolean visible = null;
         public final Integer delta = null;
         public final Boolean hidden = null;
-
-        private Config() {
-            // Hide the constructor.
-        }
     }
 
     public static final class Excerpt {
         public final String label = null;
         public final String text = null;
-
-        private Excerpt() {
-            // Hide the constructor.
-        }
     }
 
-    public static final class Field {
+    public static final class CalculationField extends Field {
+        public static final class CalculationValue {
+            public final String value = null;
+        }
+
+        public final CalculationField[] values = null;
+    }
+
+    public static final class CategoryField extends Field {
+        public static final class CategoryValue {
+            public static final class Value {
+                public final String status = null;
+                public final String text = null;
+                public final Long id = null;
+                public final String color = null;
+            }
+
+            public final Value value = null;
+        }
+
+        public final CategoryValue[] values = null;
+    }
+
+    public static final class ContactField extends Field {
+        public static final class ContactValue {
+            public static final class Value {
+                public final Long user_id = null;
+                public final Long space_id = null;
+                public final String type = null;
+                public final FileInfo image = null;
+                public final Long profile_id = null;
+                public final Long org_id = null;
+                public final String link = null;
+                public final String[] mail = null;
+                public final String external_id = null;
+                public final String last_seen_on = null;
+                public final String name = null;
+            }
+
+            public final Value value = null;
+        }
+
+        public final ContactValue[] values = null;
+    }
+
+    public static final class DateField extends Field {
+        public static final class DateValue {
+            public final String end = null;
+            public final String end_date = null;
+            public final String end_date_utc = null;
+            public final String end_time = null;
+            public final String end_time_utc = null;
+            public final String end_utc = null;
+            public final String start = null;
+            public final String start_date = null;
+            public final String start_date_utc = null;
+            public final String start_time = null;
+            public final String start_time_utc = null;
+            public final String start_utc = null;
+        }
+
+        public final DateValue[] values = null;
+    }
+
+    public static final class DurationField extends Field {
+        public static final class DurationValue {
+            public final Integer value = null;
+        }
+
+        public final DurationValue[] values = null;
+    }
+
+    public static final class EmbedField extends Field {
+        public static final class EmbedValue {
+            public static final class Value {
+                public final String description = null;
+                public final Integer embed_height = null;
+                public final String embed_html = null;
+                public final Long embed_id = null;
+                public final Integer embed_width = null;
+                public final String hostname = null;
+                public final String original_url = null;
+                public final String resolved_url = null;
+                public final String title = null;
+                public final String type = null;
+                public final String url = null;
+            }
+
+            public final Value embed = null;
+            public final FileInfo file = null;
+        }
+
+        public final EmbedValue[] values = null;
+    }
+
+    public static class Field {
         public final Config config = null;
         public final String external_id = null;
         public final Long field_id = null;
         public final String label = null;
         public final String status = null;
         public final String type = null;
-        public final Value[] values = null;
+    }
 
-        private Field() {
-            // Hide the constructor.
+    public static final class ImageField extends Field {
+        public static final class ImageValue {
+            public static final class Value {
+                public final String mimetype = null;
+                public final String perma_link = null;
+                public final String hosted_by = null;
+                public final String description = null;
+                public final String hosted_by_humanized_name = null;
+                public final Integer size = null;
+                public final String thumbnail_link = null;
+                public final String link = null;
+                public final Long file_id = null;
+                public final String link_target = null;
+                public final String name = null;
+            }
+
+            public final Value value = null;
         }
+
+        public final ImageValue[] values = null;
+    }
+
+    public static final class LocationField extends Field {
+        public static final class LocationValue {
+            public final String city = null;
+            public final String street_number = null;
+            public final String country = null;
+            public final String street_name = null;
+            public final String formatted = null;
+            public final String value = null;
+            public final String state = null;
+            public final String postal_code = null;
+            public final Double lat = null;
+            public final Double lng = null;
+        }
+
+        public final LocationValue[] values = null;
+    }
+
+    public static final class NumberField extends Field {
+        public static final class NumberValue {
+            public final String value = null;
+        }
+
+        public final NumberValue[] values = null;
+    }
+
+    public static final class MoneyField extends Field {
+        public static final class MoneyValue {
+            public final String currency = null;
+            public final String value = null;
+        }
+
+        public final MoneyValue[] values = null;
+    }
+
+    public static final class ProgressField extends Field {
+        public static final class ProgressValue {
+            public final Integer value = null;
+        }
+
+        public final ProgressValue[] values = null;
     }
 
     public static final class Options {
@@ -89,10 +231,6 @@ public final class Item {
         public final String text = null;
         public final Long id = null;
         public final String color = null;
-
-        private Options() {
-            // Hide the constructor.
-        }
     }
 
     public final class Revision {
@@ -102,49 +240,11 @@ public final class Item {
         public final Integer item_revision_id = null;
         public final Integer revision = null;
         public final String type = null;
-
-        private Revision() {
-            // Hide the constructor.
-        }
     }
 
     public static final class Settings {
         public final String size = null;
         public final Boolean multiple = null;
-
-        private Settings() {
-            // Hide the constructor.
-        }
-    }
-
-    public static final class Value {
-        public final ObjectValue value = null;
-
-        private Value() {
-            // Hide the constructor.
-        }
-    }
-
-    public static final class ObjectValue {
-        // Date/time fields.
-        public final String end = null;
-        public final String end_date = null;
-        public final String end_time = null;
-        public final String start = null;
-        public final String start_date = null;
-        public final String start_time = null;
-
-        // Money fields.
-        public final Float value = null;
-        public final String currency = null;
-
-        // Embed fields
-        public final String embed = null;
-        public final String file = null;
-
-        private ObjectValue() {
-            // Hide the constructor.
-        }
     }
 
     public final Activity[] activity = null;
@@ -188,9 +288,4 @@ public final class Item {
     public final String title = null;
     public final RatingInfo user_ratings = null;
     public final ValueInfo values = null;
-
-    private Item() {
-        // Hide the constructor.
-    }
-
 }
