85805d9684242ef1f73deca76eddae5a3d7db66b
Urszuly, László <urszuly.laszlo@hoisthq.com>
2014-05-13 20:30:51 +0200

    Introduced the Organization Role and Type enums (instead of strings).

--- sdk/src/com/podio/sdk/domain/Organization.java
+++ sdk/src/com/podio/sdk/domain/Organization.java
@@ -19,20 +19,27 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
  *  SOFTWARE.
  */
-
 package com.podio.sdk.domain;
 
 public final class Organization {
+    public static enum Type {
+        free, sponsored, premium
+    }
+
+    public static enum Role {
+        admin, regular, light
+    }
+
     public final Integer grants_count = null;
     public final Long logo = null;
     public final String name = null;
     public final Long org_id = null;
     public final Integer rank = null;
     public final String[] rights = null;
-    public final String role = null;
+    public final Role role = null;
     public final Space[] spaces = null;
-    public final String type = null;
     public final Status status = null;
+    public final Type type = null;
     public final String url = null;
     public final String url_label = null;
 }
