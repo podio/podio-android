4c8e15b35272269448ccff5867916c1cb237547e
Urszuly, László <urszuly.laszlo@hoisthq.com>
2014-06-23 14:18:38 +0200

    Cleaned up the 'User' domain object.

--- sdk/src/com/podio/sdk/domain/User.java
+++ sdk/src/com/podio/sdk/domain/User.java
@@ -22,15 +22,144 @@
 
 package com.podio.sdk.domain;
 
-public final class User {
-    public final Long user_id = null;
-    public final String name = null;
-    public final String url = null;
-    public final String type = null;
-    public final File image = null;
-    public final String avatar_type = null;
-    public final Long avatar = null;
-    public final Long id = null;
-    public final Long avatar_id = null;
-    public final String last_seen_on = null;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.podio.sdk.internal.utils.Utils;
+
+public class User {
+    private static final transient SimpleDateFormat FORMATTER_DATETIME = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static enum Flag {
+        god, bulletin_author, app_store_manager, experiment_manager,
+        org_viewer, org_manager, api_manager, extension_manager,
+        tnol_manager, seo_manager, out_of_office, sales, sales_default,
+        sales_large, sales_contract, undefined
+    }
+
+    public static enum Status {
+        inactive, active, deleted, blocked, blacklisted, undefined
+    }
+
+    public static class Email {
+        private final Boolean disabled = null;
+        private final Boolean primary = null;
+        private final Boolean verified = null;
+        private final String mail = null;
+
+        public String getAddress() {
+            return mail;
+        }
+
+        public boolean isDisabled() {
+            return Utils.getNative(disabled, false);
+        }
+
+        public boolean isPrimary() {
+            return Utils.getNative(primary, false);
+        }
+
+        public boolean isVerified() {
+            return Utils.getNative(verified, false);
+        }
+    }
+
+    private final Integer user_id = null;
+    private final List<Flag> flags = null;
+    private final List<Email> mails = null;
+    private final Status status = null;
+    private final String activated_on = null;
+    private final String created_on = null;
+    private final String locale = null;
+    private final String mail = null;
+    private final String timezone = null;
+
+    /**
+     * Gets the date when the user was activated.
+     * 
+     * @return A date object, or null if the date couldn't be parsed.
+     */
+    public Date getActivatedDate() {
+        Date date;
+
+        try {
+            date = FORMATTER_DATETIME.parse(activated_on);
+        } catch (ParseException e) {
+            date = null;
+        } catch (NullPointerException e) {
+            date = null;
+        }
+
+        return date;
+    }
+
+    public String getActivatedDateString() {
+        return activated_on;
+    }
+
+    /**
+     * Gets the date when the user was created.
+     * 
+     * @return A date object, or null if the date couldn't be parsed.
+     */
+    public Date getCreatedDate() {
+        Date date;
+
+        try {
+            date = FORMATTER_DATETIME.parse(created_on);
+        } catch (ParseException e) {
+            date = null;
+        } catch (NullPointerException e) {
+            date = null;
+        }
+
+        return date;
+    }
+
+    public String getCreatedDateString() {
+        return created_on;
+    }
+
+    public String getEmailAddress() {
+        return mail;
+    }
+
+    public List<Email> getEmails() {
+        return mails != null ?
+                new ArrayList<Email>(mails) :
+                new ArrayList<Email>();
+    }
+
+    public int getId() {
+        return Utils.getNative(user_id, -1);
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public Status getStatus() {
+        return status != null ? status : Status.undefined;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public boolean hasFlags(Flag... flags) {
+        if (this.flags != null) {
+            for (Flag flag : flags) {
+                if (!this.flags.contains(flag)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
 }
