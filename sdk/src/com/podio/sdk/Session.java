990901b8d2e83207a1de123bb4b43b1e237770d6
László Urszuly <urszuly.laszlo@gmail.com>
2014-10-06 16:53:16 +0200

    Introduced the v2 client interface.
    The Request implementation is now responsible for delegating the
    HTTP request to the Volley library (previously it was the job of
    the client implementation).
    
    The Client implementation is however still responsible for
    authentication requests, which is reflected through the client
    interface now (previously the responsibility was exposed through
    a special-case request object).
    
    The client has also been reduced in complexity as the request
    queue and worker thread spawning has been removed from it. The
    Volley library provides for both features which is relied upon
    now. This removes an entire layer of thread and enables a much
    leaner implementation which is much easier to debug.

---
+++ sdk/src/com/podio/sdk/Session.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (C) 2014 Copyright Citrix Systems, Inc.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ *  this software and associated documentation files (the "Software"), to deal in 
+ *  the Software without restriction, including without limitation the rights to 
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ *  of the Software, and to permit persons to whom the Software is furnished to 
+ *  do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all 
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ *  SOFTWARE.
+ */
+
+package com.podio.sdk;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.podio.sdk.internal.Utils;
+
+class Session {
+    private static String accessToken;
+    private static String refreshToken;
+    private static long expires;
+
+    static String accessToken() {
+        return accessToken;
+    }
+
+    static long expires() {
+        return expires;
+    }
+
+    static String refreshToken() {
+        return refreshToken;
+    }
+
+    static void set(String jsonString) {
+        JSONObject jsonObject = null;
+
+        try {
+            jsonObject = new JSONObject(jsonString);
+            accessToken = jsonObject.optString("access_token", null);
+            refreshToken = jsonObject.optString("refresh_token", null);
+
+            if (jsonObject.has("expires")) {
+                expires = jsonObject.optLong("expires", 0L);
+            } else if (jsonObject.has("expires_in")) {
+                long expiresIn = jsonObject.optLong("expires_in", 0L);
+                expires = Utils.currentTimeSeconds() + expiresIn;
+            }
+        } catch (JSONException e) {
+            // Input JSON was most likely invalid. Fallback to defaults.
+            accessToken = refreshToken = null;
+            expires = 0;
+        } catch (NullPointerException e) {
+            // Input JSON was most likely a null pointer. Fallback to defaults.
+            accessToken = refreshToken = null;
+            expires = 0;
+        }
+    }
+
+    static void set(String accessToken, String refreshToken, long expires) {
+        Session.accessToken = accessToken;
+        Session.refreshToken = refreshToken;
+        Session.expires = expires;
+    }
+
+    private Session() {
+    }
+}
---
+++ sdk/src/com/podio/sdk/VolleyClient.java
@@ -0,0 +1,224 @@
+/*
+ *  Copyright (C) 2014 Copyright Citrix Systems, Inc.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ *  this software and associated documentation files (the "Software"), to deal in 
+ *  the Software without restriction, including without limitation the rights to 
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ *  of the Software, and to permit persons to whom the Software is furnished to 
+ *  do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all 
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ *  SOFTWARE.
+ */
+package com.podio.sdk;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import javax.net.ssl.SSLSocketFactory;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.Volley;
+import com.podio.sdk.internal.Utils;
+
+class VolleyClient implements Client, PodioRequest.ErrorListener, PodioRequest.ResultListener<Void> {
+
+    private static class AuthPath extends Filter {
+
+        private AuthPath() {
+            super("oauth/token");
+        }
+
+        private AuthPath withClientCredentials(String clientId, String clientSecret) {
+            addQueryParameter("client_id", clientId);
+            addQueryParameter("client_secret", clientSecret);
+            return this;
+        }
+
+        private AuthPath withUserCredentials(String username, String password) {
+            addQueryParameter("grant_type", "password");
+            addQueryParameter("username", username);
+            addQueryParameter("password", password);
+            return this;
+        }
+
+        private AuthPath withAppCredentials(String appId, String appToken) {
+            addQueryParameter("grant_type", "app");
+            addQueryParameter("app_id", appId);
+            addQueryParameter("app_token", appToken);
+            return this;
+        }
+
+        private AuthPath withRefreshToken(String refreshToken) {
+            addQueryParameter("grant_type", "refresh_token");
+            addQueryParameter("refresh_token", refreshToken);
+            return this;
+        }
+
+    }
+
+    private String scheme;
+    private String authority;
+    private RequestQueue volleyRequestQueue;
+    private RequestQueue volleySessionQueue;
+    private String clientId;
+    private String clientSecret;
+    private boolean isRefreshing;
+    private VolleyRequest<?> currentRequest;
+
+    @Override
+    public PodioRequest<Void> authenticateWithUserCredentials(String username, String password) {
+        Uri uri = new AuthPath()
+                .withClientCredentials(clientId, clientSecret)
+                .withUserCredentials(username, password)
+                .buildUri(scheme, authority);
+
+        String url = parseUrl(uri);
+        HashMap<String, String> params = parseParams(uri);
+        VolleyRequest<Void> request = VolleyRequest.newAuthRequest(url, params);
+        volleyRequestQueue.add(request);
+
+        return request;
+    }
+
+    @Override
+    public PodioRequest<Void> authenticateWithAppCredentials(String appId, String appToken) {
+        Uri uri = new AuthPath()
+                .withClientCredentials(clientId, clientSecret)
+                .withAppCredentials(appId, appToken)
+                .buildUri(scheme, authority);
+
+        String url = parseUrl(uri);
+        HashMap<String, String> params = parseParams(uri);
+        VolleyRequest<Void> request = VolleyRequest.newAuthRequest(url, params);
+        volleyRequestQueue.add(request);
+
+        return request;
+    }
+
+    @Override
+    public boolean onRequestPerformed(Void nothing) {
+        currentRequest.setSessionChanged(true);
+        volleySessionQueue.add(currentRequest);
+        volleyRequestQueue.start();
+        return true;
+    }
+
+    @Override
+    public boolean onErrorOccured(Throwable cause) {
+        if (isRefreshing) {
+            // An attempt to refresh the session has been made, but it failed.
+            // Clear the request queue as all pending requests will fail without
+            // a valid session.
+            volleyRequestQueue.stop();
+
+            VolleyError error;
+
+            if (cause instanceof VolleyError) {
+                error = (VolleyError) cause;
+            } else if (cause.getCause() instanceof VolleyError) {
+                error = (VolleyError) cause.getCause();
+            } else {
+                error = new VolleyError(cause);
+            }
+
+            currentRequest.removeErrorListener(this);
+            currentRequest.deliverError(error);
+
+            return true;
+        } else if (cause instanceof PodioError && ((PodioError) cause).getStatusCode() == 401) {
+            // The request has failed due to a 401 server response, try to
+            // resolve the issue by refreshing the session.
+            isRefreshing = true;
+            volleyRequestQueue.stop();
+
+            Uri uri = new AuthPath()
+                    .withClientCredentials(clientId, clientSecret)
+                    .withRefreshToken(Session.refreshToken())
+                    .buildUri(scheme, authority);
+
+            String url = parseUrl(uri);
+            HashMap<String, String> params = parseParams(uri);
+            VolleyRequest<Void> request = VolleyRequest
+                    .newAuthRequest(url, params)
+                    .withContentListener(this)
+                    .withErrorListener(this);
+            volleySessionQueue.add(request);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public <T> PodioRequest<T> request(PodioRequest.Method method, Filter filter, Object item, Class<T> classOfItem) {
+        Uri uri = filter.buildUri(scheme, authority);
+        String url = parseUrl(uri);
+        String body = item != null ? JsonParser.toJson(item) : null;
+
+        VolleyRequest<T> request = VolleyRequest.newRequest(method, url, body, classOfItem);
+        request.withErrorListener(this);
+        volleyRequestQueue.add(request);
+
+        return request;
+    }
+
+    void setup(Context context, String scheme, String authority, String clientId, String clientSecret, SSLSocketFactory sslSocketFactory) {
+        this.scheme = scheme;
+        this.authority = authority;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+
+        if (sslSocketFactory == null) {
+            this.volleyRequestQueue = Volley.newRequestQueue(context);
+            this.volleySessionQueue = Volley.newRequestQueue(context);
+        } else {
+            HurlStack stack = new HurlStack(null, sslSocketFactory);
+            this.volleyRequestQueue = Volley.newRequestQueue(context, stack);
+            this.volleySessionQueue = Volley.newRequestQueue(context, stack);
+        }
+    }
+
+    private HashMap<String, String> parseParams(Uri uri) {
+        Set<String> keys = uri.getQueryParameterNames();
+        HashMap<String, String> params = new HashMap<String, String>();
+
+        for (String key : keys) {
+            String value = uri.getQueryParameter(key);
+            params.put(key, value);
+        }
+
+        return params;
+    }
+
+    private String parseUrl(Uri uri) {
+        if (Utils.isEmpty(uri)) {
+            return null;
+        }
+
+        String url = uri.toString();
+        int queryStart = url.indexOf("?");
+
+        if (queryStart > 0) {
+            url = url.substring(0, queryStart);
+        }
+
+        return url;
+    }
+
+}
