55c17b2f27cb545b63b5727b185794fccb691712
László Urszuly <urszuly.laszlo@gmail.com>
2014-12-02 22:23:27 +0100

    Make Presence domain object Parcelable.

--- sdk/src/com/podio/sdk/domain/Presence.java
+++ sdk/src/com/podio/sdk/domain/Presence.java
@@ -21,16 +21,63 @@
  */
 package com.podio.sdk.domain;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.podio.sdk.internal.Utils;
 
-public class Presence {
-    private final String ref_type = null;
-    private final Long ref_id = null;
-    private final Long user_id = null;
-    private final String signature = null;
+public class Presence implements Parcelable {
 
-    public String getRef_type() {
-        return ref_type;
+    public static final Parcelable.Creator<Presence> CREATOR = new Parcelable.Creator<Presence>() {
+        public Presence createFromParcel(Parcel in) {
+            return new Presence(in);
+        }
+
+        public Presence[] newArray(int size) {
+            return new Presence[size];
+        }
+    };
+
+    private final Long ref_id;
+    private final Long user_id;
+    private final String ref_type;
+    private final String signature;
+
+    private Presence(Parcel parcel) {
+        this.ref_id = parcel.readLong();
+        this.user_id = parcel.readLong();
+        this.ref_type = parcel.readString();
+        this.signature = parcel.readString();
+    }
+
+    private Presence() {
+        this.ref_id = null;
+        this.user_id = null;
+        this.ref_type = null;
+        this.signature = null;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(ref_id);
+        dest.writeLong(user_id);
+        dest.writeString(ref_type);
+        dest.writeString(signature);
+    }
+
+    public Type getRefType() {
+        try {
+            return Type.valueOf(ref_type);
+        } catch (NullPointerException e) {
+            return Type.unknown;
+        } catch (IllegalArgumentException e) {
+            return Type.unknown;
+        }
     }
 
     public long getRefId() {
@@ -44,4 +91,5 @@ public class Presence {
     public String getSignature() {
         return signature;
     }
+
 }
