3b22e6522b48d4cbacbb193d43dc2869df8509ae
RabieJradi <jradi88@gmail.com>
2015-02-15 23:34:45 +0100

    modified space and org domains to adapt for workspace archive feature

--- sdk/src/main/java/com/podio/sdk/domain/Space.java
+++ sdk/src/main/java/com/podio/sdk/domain/Space.java
@@ -1,22 +1,22 @@
 /*
  *  Copyright (C) 2014 Copyright Citrix Systems, Inc.
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of 
- *  this software and associated documentation files (the "Software"), to deal in 
- *  the Software without restriction, including without limitation the rights to 
- *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
- *  of the Software, and to permit persons to whom the Software is furnished to 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in
+ *  the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to
  *  do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in all 
+ *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  SOFTWARE.
  */
 
@@ -45,6 +45,7 @@ public class Space {
         return new Space();
     }
 
+    private final Boolean archived = null;
     private final Boolean auto_join = null;
     private final Boolean post_on_new_app = null;
     private final Boolean post_on_new_member = null;
@@ -73,6 +74,10 @@ public class Space {
         return Utils.getNative(auto_join, false);
     }
 
+    public boolean isArchived() {
+        return Utils.getNative(archived, false);
+    }
+
     public boolean doPostOnNewApp() {
         return Utils.getNative(post_on_new_app, false);
     }
@@ -87,7 +92,7 @@ public class Space {
 
     /**
      * Gets the end date of the calendar event as a Java Date object.
-     * 
+     *
      * @return A date object, or null if the date couldn't be parsed.
      */
     public Date getCreatedDate() {
@@ -145,7 +150,7 @@ public class Space {
     /**
      * Checks whether the list of rights the user has for this application
      * contains <em>all</em> the given permissions.
-     * 
+     *
      * @param permissions
      *        The list of permissions to check for.
      * @return Boolean true if all given permissions are found or no permissions
