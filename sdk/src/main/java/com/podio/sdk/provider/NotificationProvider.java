1a3623e5e4ba3c54ea726f980b8fabce4c471d73
RabieJradi <jradi88@gmail.com>
2015-02-20 00:57:13 +0100

    Added support for GET notification API call (notification list)

--- sdk/src/main/java/com/podio/sdk/provider/NotificationProvider.java
+++ sdk/src/main/java/com/podio/sdk/provider/NotificationProvider.java
@@ -25,6 +25,8 @@ import com.podio.sdk.Filter;
 import com.podio.sdk.Provider;
 import com.podio.sdk.Request;
 import com.podio.sdk.domain.NotificationGroup;
+import com.podio.sdk.domain.ReferenceType;
+import com.podio.sdk.domain.notification.Notification;
 
 /**
  * Enables access to the NotificationGroup API end point.
@@ -33,21 +35,171 @@ import com.podio.sdk.domain.NotificationGroup;
  */
 public class NotificationProvider extends Provider {
 
-    static class Path extends Filter {
+    private static class NotificationFilter extends Filter {
 
-        protected Path() {
+        protected NotificationFilter() {
             super("notification");
         }
 
-        Path withId(long id) {
+        public NotificationFilter withId(long id) {
             addPathSegment(Long.toString(id, 10));
             return this;
         }
 
-        Path withViewed(){
+        public NotificationFilter withViewed(){
             addPathSegment("viewed");
             return this;
         }
+    }
+
+    /**
+     * This builder class is used in order to specify which
+     * parameters to send in our API request for notifications
+     *
+     * @author rabie
+     */
+    public static class GetNotificationFilter extends Filter{
+
+
+        public static enum Viewed{
+            ALL,UNVIEWED_ONLY,VIEWED_ONLY;
+        }
+
+        public static enum Direction{
+            INCOMING,OUTGOING;
+        }
+
+        public static enum Starred{
+            STARRED,UNSTARRED,BOTH
+        }
+
+        public GetNotificationFilter(){
+            super("notification");
+        }
+
+        /**
+         * The type of the context to get notifications, e.g. "conversation", "item" or "task", etc.
+         *
+         * @param referenceType the type of reference
+         * @return
+         */
+        public GetNotificationFilter contextType(ReferenceType referenceType){
+            addQueryParameter("context_type",referenceType.name());
+            return this;
+        }
+
+        // TODO support SDK operation
+        public GetNotificationFilter createdFrom(){
+            throw new UnsupportedOperationException("not implemented yet");
+        }
+
+        // TODO support SDK operation
+        public GetNotificationFilter createdTo(){
+            throw new UnsupportedOperationException("not implemented yet");
+        }
+
+        /**
+         * "incoming" to get incoming notifications, "outgoing" to get outgoing
+         * notifications. Default value: incoming.
+         *
+         * @param direction
+         */
+        public GetNotificationFilter direction(Direction direction){
+            switch (direction){
+                case INCOMING:
+                    addQueryParameter("direction","incoming");
+                    break;
+                case OUTGOING:
+                    addQueryParameter("direction","outgoing");
+                    break;
+            }
+            return this;
+        }
+
+        /**
+         * Set the maximum number of notifications to return, maximum is "100". Default value: 20
+         * @param value the value of the limit
+         * @return
+         */
+        public GetNotificationFilter limit(int value){
+            addQueryParameter("limit",Integer.toString(value));
+            return this;
+        }
+
+        /**
+         * Set the offset into the returned notifications. Default value: 0
+         * @param value the value of the offset
+         * @return
+         */
+        public GetNotificationFilter offset(int value){
+            addQueryParameter("offset",Integer.toString(value));
+            return this;
+        }
+
+        /**
+         *
+         * @param starred default behaviour if not specified is BOTH,
+         *               which means both starred and unstarred notifications is provided.
+         * @return
+         */
+        public GetNotificationFilter starred(Starred starred){
+            switch (starred){
+                case BOTH:
+                    //do nothing - leave blank
+                    break;
+                case STARRED:
+                    addQueryParameter("starred","true");
+                    break;
+                case UNSTARRED:
+                    addQueryParameter("starred","false");
+                    break;
+            }
+            return this;
+        }
+
+        public GetNotificationFilter type(Notification.NotificationType type){
+            addQueryParameter("type",type.name());
+            return this;
+        }
+
+        /**
+         * Set the user id of the other part of the notification
+         * @param userId
+         * @return
+         */
+        public GetNotificationFilter userId(long userId){
+            addQueryParameter("user_id",Long.toString(userId));
+            return this;
+        }
+
+        /**
+         *
+         * @param viewed default behaviour if not specified is ALL,
+         *               which means both unread and read notifications is provided.
+         * @return
+         */
+        public GetNotificationFilter viewed(Viewed viewed){
+            switch (viewed){
+                case ALL:
+                    //do nothing - leave blank
+                    break;
+                case UNVIEWED_ONLY:
+                    addQueryParameter("viewed","false");
+                    break;
+                case VIEWED_ONLY:
+                    addQueryParameter("viewed","true");
+                    break;
+            }
+            return this;
+        }
+
+        // TODO support SDK operation
+        public GetNotificationFilter viewedFrom(){
+            throw new UnsupportedOperationException("not implemented yet");
+        }
+
+
+
     }
 
     /**
@@ -56,8 +208,12 @@ public class NotificationProvider extends Provider {
      * @return A ticket which the caller can use to identify this request with.
      */
     public Request<NotificationGroup> getNotification(long id) {
-        Path filter = new Path().withId(id);
+        NotificationFilter filter = new NotificationFilter().withId(id);
         return get(filter, NotificationGroup.class);
+    }
+
+    public Request<NotificationGroup[]> getNotifications(GetNotificationFilter filter){
+        return get(filter, NotificationGroup[].class);
     }
 
     /**
@@ -65,7 +221,7 @@ public class NotificationProvider extends Provider {
      * @return A ticket which the caller can use to identify this request with.
      */
     public Request<Void> markAllNotificationsAsViewed() {
-        Path filter = new Path().withViewed();
+        NotificationFilter filter = new NotificationFilter().withViewed();
         return post(filter,null,Void.class);
 
     }
@@ -76,7 +232,7 @@ public class NotificationProvider extends Provider {
      * @return A ticket which the caller can use to identify this request with.
      */
     public Request<Void> markNotificationAsViewed(long notificationId) {
-        Path filter = new Path().withId(notificationId).withViewed();
+        NotificationFilter filter = new NotificationFilter().withId(notificationId).withViewed();
         return post(filter,null,Void.class);
 
     }
