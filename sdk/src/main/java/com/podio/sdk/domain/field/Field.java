32385f615c9890c788f49e6ec3dd0c38df0a79a3
RabieJradi <jradi88@gmail.com>
2015-08-05 10:36:03 +0200

    added the link field type

--- sdk/src/main/java/com/podio/sdk/domain/field/Field.java
+++ sdk/src/main/java/com/podio/sdk/domain/field/Field.java
@@ -145,6 +145,7 @@ public abstract class Field<T extends Field.Value> {
         text(TextField.class),
         email(EmailField.class),
         phone(PhoneField.class),
+        link(LinkField.class),
         undefined(UndefinedField.class);
 
         private final Class<? extends Field> fieldClass;
