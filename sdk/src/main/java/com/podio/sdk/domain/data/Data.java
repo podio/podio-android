a6fa791d9b8f6a0b9e03654c05fa3e6efd0c7edf
RabieJradi <jradi88@gmail.com>
2015-10-05 17:07:12 +0200

    added javadoc for the data interface

--- sdk/src/main/java/com/podio/sdk/domain/data/Data.java
+++ sdk/src/main/java/com/podio/sdk/domain/data/Data.java
@@ -23,6 +23,9 @@
 package com.podio.sdk.domain.data;
 
 /**
+ * Interface that defines the static inheritance structure of the supported types of reference data
+ * objects that our custom reference data JSON deserializer can parse.
+ *
  * @author rabie
  */
 public interface Data {
