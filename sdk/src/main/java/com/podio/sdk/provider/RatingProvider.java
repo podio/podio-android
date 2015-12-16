842485ec2bb5d778f874c196d3b806a486db281a
RabieJradi <jradi88@gmail.com>
2015-12-16 14:10:48 +0100

    added support for "Get who liked an object" API call

--- sdk/src/main/java/com/podio/sdk/provider/RatingProvider.java
+++ sdk/src/main/java/com/podio/sdk/provider/RatingProvider.java
@@ -4,12 +4,12 @@ package com.podio.sdk.provider;
 import com.podio.sdk.Filter;
 import com.podio.sdk.Provider;
 import com.podio.sdk.Request;
+import com.podio.sdk.domain.Profile;
 import com.podio.sdk.domain.Rating;
 import com.podio.sdk.domain.ReferenceType;
 
 /**
  * Enables access to the Ratings API end point.
- *
  */
 public class RatingProvider extends Provider {
 
@@ -26,15 +26,19 @@ public class RatingProvider extends Provider {
             return this;
         }
 
-        public RatingFilter withRatingType(Rating.RatingType ratingType){
+        public RatingFilter withRatingType(Rating.RatingType ratingType) {
             this.addPathSegment(ratingType.name());
 
+            return this;
+        }
+
+        public RatingFilter likedBy() {
+            this.addPathSegment("liked_by");
             return this;
         }
     }
 
     /**
-     *
      * Create a like on a reference type with the given id. The request result will deliver a rating
      * object with only rating_id set.
      *
@@ -47,7 +51,7 @@ public class RatingProvider extends Provider {
      */
     public Request<Rating> createLike(ReferenceType type, long id) {
         RatingFilter filter = new RatingFilter();
-        filter.withReference(type,id);
+        filter.withReference(type, id);
         filter.withRatingType(Rating.RatingType.like);
         Rating.Create create = new Rating.Create(1);
 
@@ -55,9 +59,8 @@ public class RatingProvider extends Provider {
     }
 
     /**
-     *
-     * Create an unlike on a reference type with the given id. The request result will deliver a rating
-     * object with only rating_id set.
+     * Create an unlike on a reference type with the given id. The request result will deliver a
+     * rating object with only rating_id set.
      *
      * @param type
      *         The reference type of what you are unliking.
@@ -68,9 +71,27 @@ public class RatingProvider extends Provider {
      */
     public Request<Void> createUnlike(ReferenceType type, long id) {
         RatingFilter filter = new RatingFilter();
-        filter.withReference(type,id);
+        filter.withReference(type, id);
         filter.withRatingType(Rating.RatingType.like);
 
         return delete(filter);
+    }
+
+    /**
+     * get a list of profiles of everyone that liked the given object.
+     *
+     * @param type
+     *         The reference type of what of the object.
+     * @param id
+     *         The id of the reference type of the object.
+     *
+     * @return A ticket which the caller can use to identify this request with.
+     */
+    public Request<Profile[]> getWhoLikedAnObject(ReferenceType type, long id) {
+        RatingFilter filter = new RatingFilter();
+        filter.withReference(type, id);
+        filter.likedBy();
+
+        return get(filter, Profile[].class);
     }
 }
